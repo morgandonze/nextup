6357e1018abcc2ce8066c5b96670046c4e535d52
Morgan Donze <morganlaco@gmail.com>
2019-11-24 23:08:54 -0500

    Update index template

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -19,10 +19,61 @@ const CONTAINER = (size: number): ViewStyle => ({
 })
 
 const HEADER_CONTAINER = (size: number): ViewStyle => ({
-  backgroundColor: '#ffab00'
+  backgroundColor: '#ffd34d',
+  paddingHorizontal: 18,
+  paddingVertical: 20
+})
+
+const HEADER_TOP = (size: number): ViewStyle => ({
+  flexDirection: 'row',
+  justifyContent: 'space-between'
 })
 
 const BODY = (size: number): ViewStyle => ({})
+
+const BUN_WRAPPER = {
+  justifyContent: 'center',
+  alignItems: 'center',
+  display: 'flex'
+}
+
+const BUN = {
+  backgroundColor: '#333',
+  height: 3,
+  width: 26,
+  marginBottom: 2
+}
+
+const ROW_NAV_WRAPPER = {
+  flexDirection: 'row'
+}
+
+const NAV_ITEM = {
+  marginRight: 12
+}
+
+const MODAL = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  backgroundColor: '#111',
+  opacity: 0.97,
+  width: '100%',
+  flex: 1,
+  height: '100%',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+
+const MODAL_ITEMS = {}
+
+const MODAL_ITEM_TEXT = {
+  color: 'white'
+}
+
+const MODAL_ITEM = {
+  marginBottom: 18
+}
 
 const lorem = () => (
   <>
@@ -37,6 +88,7 @@ const lorem = () => (
       luctus et ultrices posuere cubilia Curae; Proin vitae tempor massa, non
       cursus leo. In
     </p>
+
     <p>
       neque augue, sodales vitae hendrerit at, gravida eleifend velit. Nulla
       dapibus euismod felis, quis volutpat tellus rhoncus non. Donec odio metus,
@@ -82,6 +134,46 @@ const head = () => (
   </div>
 )
 
+const bunMenu = () => (
+  <View style={BUN_WRAPPER}>
+    <View style={BUN} />
+    <View style={BUN} />
+    <View style={BUN} />
+  </View>
+)
+
+const rowMenu = () => (
+  <View style={ROW_NAV_WRAPPER}>
+    <View style={NAV_ITEM}>
+      <Text>Home</Text>
+    </View>
+    <View style={NAV_ITEM}>
+      <Text>Home</Text>
+    </View>
+    <View style={NAV_ITEM}>
+      <Text>Home</Text>
+    </View>
+  </View>
+)
+
+const modal = () => (
+  <View style={MODAL}>
+    <View style={MODAL_ITEMS}>
+      <View style={MODAL_ITEM}>
+        <Text style={MODAL_ITEM_TEXT}>ITEM</Text>
+      </View>
+
+      <View style={MODAL_ITEM}>
+        <Text style={MODAL_ITEM_TEXT}>ITEM</Text>
+      </View>
+
+      <View style={MODAL_ITEM}>
+        <Text style={MODAL_ITEM_TEXT}>ITEM</Text>
+      </View>
+    </View>
+  </View>
+)
+
 export default function Index() {
   const smallSize = useMediaPredicate('(min-width: 576px)')
   const mediumSize = useMediaPredicate('(min-width: 768px)')
@@ -93,12 +185,19 @@ export default function Index() {
     <View style={CONTAINER(size)}>
       {head()}
       <View style={HEADER_CONTAINER(size)}>
-        <Text style={{ fontSize: (1 + size) * 10 + 20 }}>Title</Text>
+        <View style={HEADER_TOP(size)}>
+          <Text style={{ fontSize: (1 + size) * 10 + 20 }}>Title</Text>
+          {size <= 1 && bunMenu()}
+        </View>
+
+        {size > 1 && rowMenu()}
       </View>
 
       <View style={BODY(size)}>
         <Text style={{ fontSize: (1 + size) * 2 + 14 }}>{lorem()}</Text>
       </View>
+
+      {false && modal()}
     </View>
   )
 }
